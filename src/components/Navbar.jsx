dec25c469df1ab9d069c20090046c100eca6a543
Megan <mennis0145@gmail.com>
2019-10-02 12:34:59 +0100

    UseLocalStorage custom hook created, in Navbar file still has to be exported

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const valueFromLocalStorage = localStorage.getItem(key);
+    if (valueFromLocalStorage) {
+      return JSON.parse(valueFromLocalStorage);
+    } else {
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    localStorage.setItem(key, JSON.stringify(value));
+    setStoredValue(value);
+  }
+
+  return [storedValue, setValue];
+};
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -12,7 +30,7 @@ const Navbar = () => {
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkMode ? "toggle toggled" : "toggle"}
         />
       </div>
     </nav>
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,7 @@
+// import React, { useState } from 'react';
+
+// const useLocalStorage = (key, initialValue) => {
+//     console.log(key, initialValue)
+// }
+
+// export default useLocalStorage;
